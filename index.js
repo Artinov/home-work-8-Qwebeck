2fa496a0c38a2d592037970c1f94ee0038956fc5
Qwebeck5 <Qwebeck5@gmail.com>
2017-02-22 00:14:51 +0300

    Finish first function

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-function setText(selector, text) {
+
+function setText(selector,text) {
 	/*
 		`selector` is string with CSS-like selector
 		`text` - is just string with text we want to change
 	*/
-
 	// Write your code here
+document.querySelector(selector).innerText = text
+
 }
 
 
-function setTextAll(selector, text) {
-	// Write your code here
+
+ function setTextAll(selector, text) {
+
+document.querySelector(selector).innerText = text
+
 }
+
 
 
 setText("p", "wasn't that hard, yeah ?");
 setTextAll("div .block a", "or was it ?");
+
